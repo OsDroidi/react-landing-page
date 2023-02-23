27a48198aebc57070994461727c561d394a73dfb
Osama Saeed <osdroidi@gmail.com>
2023-02-24 02:45:05 +0300

    Update Footer with link to author website

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { Twitter, Facebook, Instagram } from "./FooterElements";
 const Footer = () => {
   return (
     <div className="bg-dark-blue-foot  text-white text-xl">
-      <div className="flex items-center flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row  container mx-auto p-12   ">
+      <div className="flex items-center flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row  container mx-auto p-8">
         <p className="flex-auto mb-2">PRODUCT</p>
         <p className="flex-auto mb-2">PARTNERS</p>
         <p className="flex-auto mb-2">RESOURCES</p>
@@ -14,6 +14,11 @@ const Footer = () => {
         <Facebook />
         <Instagram />
       </div>
+      <small className="flex justify-center items-center pb-5">
+        <a target="_blank" rel="noreferrer" href="https://osdroidi.com/">
+          Made by Osama Mehifoodh
+        </a>
+      </small>
     </div>
   );
 };
